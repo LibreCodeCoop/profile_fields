4ba270bcae461f065a66f3b0c6b1a85ff87649b7
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-17 11:15:47 -0300

    refactor(workflow): remove legacy operation registrations
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Listener/RegisterWorkflowOperationListener.php
+++ lib/Listener/RegisterWorkflowOperationListener.php
@@ -9,12 +9,10 @@ declare(strict_types=1);
 
 namespace OCA\ProfileFields\Listener;
 
-use OCA\ProfileFields\Workflow\CreateActivityProfileFieldChangeOperation;
 use OCA\ProfileFields\Workflow\CreateTalkConversationProfileFieldChangeOperation;
 use OCA\ProfileFields\Workflow\EmailUserProfileFieldChangeOperation;
 use OCA\ProfileFields\Workflow\LogProfileFieldChangeOperation;
 use OCA\ProfileFields\Workflow\NotifyAdminsOrGroupsProfileFieldChangeOperation;
-use OCA\ProfileFields\Workflow\NotifyUserProfileFieldChangeOperation;
 use OCA\ProfileFields\Workflow\SendWebhookProfileFieldChangeOperation;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
@@ -26,10 +24,8 @@ use OCP\WorkflowEngine\Events\RegisterOperationsEvent;
 class RegisterWorkflowOperationListener implements IEventListener {
 	public function __construct(
 		private LogProfileFieldChangeOperation $operation,
-		private NotifyUserProfileFieldChangeOperation $notifyUserOperation,
 		private EmailUserProfileFieldChangeOperation $emailUserOperation,
 		private NotifyAdminsOrGroupsProfileFieldChangeOperation $notifyAdminsOrGroupsOperation,
-		private CreateActivityProfileFieldChangeOperation $createActivityOperation,
 		private CreateTalkConversationProfileFieldChangeOperation $createTalkConversationOperation,
 		private SendWebhookProfileFieldChangeOperation $sendWebhookOperation,
 	) {
@@ -42,10 +38,8 @@ class RegisterWorkflowOperationListener implements IEventListener {
 		}
 
 		$event->registerOperation($this->operation);
-		$event->registerOperation($this->notifyUserOperation);
 		$event->registerOperation($this->emailUserOperation);
 		$event->registerOperation($this->notifyAdminsOrGroupsOperation);
-		$event->registerOperation($this->createActivityOperation);
 		$event->registerOperation($this->createTalkConversationOperation);
 		$event->registerOperation($this->sendWebhookOperation);
 	}
