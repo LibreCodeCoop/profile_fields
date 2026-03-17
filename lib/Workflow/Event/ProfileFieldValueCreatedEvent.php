1e7093e1c142cc4248751b74074b5758ab663da0
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 21:19:11 -0300

    feat(workflow): add profile field created event
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ lib/Workflow/Event/ProfileFieldValueCreatedEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2026 LibreCode coop and LibreCode contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\ProfileFields\Workflow\Event;
+
+final class ProfileFieldValueCreatedEvent extends AbstractProfileFieldValueEvent {
+}
