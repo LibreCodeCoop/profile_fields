ed3239fb74a34f25679272f2c2b04853ada04bc6
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-18 16:18:41 -0300

    fix(test): pass dispatcher instead of manager to boot context callback
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/ControllerIntegration/Workflow/LogProfileFieldChangeOperationTest.php
+++ tests/php/ControllerIntegration/Workflow/LogProfileFieldChangeOperationTest.php
@@ -207,7 +207,7 @@ class LogProfileFieldChangeOperationTest extends TestCase {
 		$bootContext->expects($this->any())
 			->method('injectFn')
 			->willReturnCallback(function (callable $fn) use ($container, $generalLogger): mixed {
-				return $fn($this->workflowManager, $container, $generalLogger);
+				return $fn($this->dispatcher, $container, $generalLogger);
 			});
 		$workflowApp->boot($bootContext);
 	}
